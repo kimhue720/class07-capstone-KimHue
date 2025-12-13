b2532096bc76fb40671512a935cd04e3e86f003b
PC <kimhuehoang.vta@gmail.com>
2025-12-13 20:36:03 +0700

    test login

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,54 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends CommonPage {
+
+    private By byTxtAccountLogin = By.id("taiKhoan");;
+    private By byTxtPasswordLogin = By.id("matKhau");;
+    private By byBtnLogin = By.xpath("//button[.='Đăng nhập']");;
+    private By byLblLoginMsg = By.id("swal2-title");;
+    private By byLblLoginfailMsg = By.xpath("//p[@id='matKhau-helper-text']");;
+    private By byLblLoginfailaccountMsg = By.xpath("//p[@id='taiKhoan-helper-text']");;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void enterAccount(String account) {
+        LOG.info("enterAccount: " + account);
+        sendKeys(byTxtAccountLogin, account);
+    }
+
+    public void enterPassword(String password) {
+        LOG.info("enterPassword: " + password);
+        sendKeys(byTxtPasswordLogin, password);
+    }
+
+    public void clickLogin() {
+        LOG.info("clickLogin");
+        click(byBtnLogin);
+    }
+
+    public String getMessage() {
+        LOG.info("getMessage");
+        return getText(byLblLoginMsg);
+    }
+
+    public String getMessageErrorPassword() {
+        LOG.info("getMessageFail");
+        return getText(byLblLoginfailMsg);
+    }
+
+    public String getMessageErrorAccount() {
+        LOG.info("getMessageFailAccount");
+        return getText(byLblLoginfailaccountMsg);
+    }
+
+    public void login(String account, String password) {
+        enterAccount(account);
+        enterPassword(password);
+        clickLogin();
+    }
+}
---
+++ src/test/java/testcases/login/TC04_LoginTestFailAccount.java
@@ -0,0 +1,42 @@
+package testcases.login;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.*;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class TC04_LoginTestFailAccount extends BaseTest {
+
+    //class variable
+    LoginPage loginPage;
+    HomePage homePage;
+
+    @Test
+    public void testinValidLogin() {
+        loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
+
+        //Step 1: Go to https://demo1.cybersoft.edu.vn
+        System.out.println("driver = " + driver);
+        Reporter.log("Step 1: Go to https://demo1.cybersoft.edu.vn");
+        driver.get("https://demo1.cybersoft.edu.vn");
+
+        //Step 2: Click 'Đăng Nhập' link
+        homePage.navigateLoginPage();
+
+        //Step 3: Enter password
+        loginPage.enterPassword("123456");
+
+        //Step 4: Click login
+        loginPage.clickLogin();
+
+        //Step 5: Verify login fail
+        //VP1: "Đây là trường bắt buộc !" message displays
+        String actualLoginfailaccountMsg = loginPage.getMessageErrorAccount();
+        Assert.assertEquals(actualLoginfailaccountMsg, "Đây là trường bắt buộc !", "Login fail");
+
+    }
+
+}
